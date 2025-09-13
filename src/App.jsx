6e460ad1a12720164e7cd912a17784aea1d8892e
Kausar Hossain Bidyut <khbidyut31@gmail.com>
2025-09-13 21:46:29 +0600

    add bidyou 2 code

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
       <h1>this is an edit in feature 1     branch </h1>
       <h1>this is an edit in feature 2     branch </h1>
       <h1>This is Bidyut code</h1>
+      <p>another code add bidyut</p>
     </>
   )
 }
